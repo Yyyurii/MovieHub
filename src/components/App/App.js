0536b3ba82119e9e0d9f02135b9d39024ca140ec
Yyyurii <yurii.kryvko@gmail.com>
2022-07-02 23:39:23 +0300

    Completed preparatory work

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './app.scss';
+
+const App = () => {
+  return (
+    <>
+      <header className="header">Hello</header>
+      <main className="main">Main</main>
+      <footer className="footer">footer</footer>
+    </>
+  );
+};
+
+export default App;
