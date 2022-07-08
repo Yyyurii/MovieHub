807342deebd101ec26ebae5083bab24e144a6ed0
Yyyurii <yurii.kryvko@gmail.com>
2022-07-08 21:24:15 +0300

    Created adaptive layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,9 +4,11 @@ import userImg from "../../assets/images/icon/photo-cover.svg";
 import logo from "../../assets/images/icon/logo.svg";
 import playBtn from "../../assets/images/previews/play-button.svg";
 import trailer1 from "../../assets/images/previews/01.jpg";
-import trailer2 from "../../assets/images/previews/02.jpg";
-import trailer3 from "../../assets/images/previews/03.jpg";
-import trailer4 from "../../assets/images/previews/04.jpg";
+// import trailer2 from "../../assets/images/previews/02.jpg";
+// import trailer3 from "../../assets/images/previews/03.jpg";
+// import trailer4 from "../../assets/images/previews/04.jpg";
+import duration from "../../assets/images/icon/duration.svg";
+import views from "../../assets/images/icon/views.svg";
 
 const App = () => {
   return (
@@ -94,13 +96,13 @@ const App = () => {
                 </div>
               </div>
               <div className="previews__trailers">
-                <span>Trailers</span>
+                <p>Trailers</p>
                 <div className="previews__gallery gallery">
                   <div className="gallery__item">
                     <span>01</span>
                     <img src={trailer1} alt="trailers" />
                   </div>
-                  <div className="gallery__item">
+                  {/* <div className="gallery__item">
                     <span>02</span>
                     <img src={trailer2} alt="trailers" />
                   </div>
@@ -111,7 +113,7 @@ const App = () => {
                   <div className="gallery__item">
                     <span>04</span>
                     <img src={trailer4} alt="trailers" />
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
@@ -119,8 +121,97 @@ const App = () => {
         </div>
       </header>
 
-      <main className="main"></main>
-      <footer className="footer">footer</footer>
+      <main className="main">
+        <div className="tab-section">
+          <div className="container">
+            <div className="tab-section__header">
+              <a href="/" className="tab-section__title">Top Movies</a>
+              <button>View More</button>
+            </div>
+            <div className="tab-section__cards">
+              <a href="/" className="card card_wider">
+                <div className="card__genre">Fantasy</div>
+                <div className="card__details">
+                  <div className="card__duration">
+                    <img src={duration} alt="duration" />
+                    <span>1hr 24mins</span>
+                  </div>
+                  <div className="card__views">
+                    <img src={views} alt="views" />
+                    <span>21.3K views</span>
+                  </div>
+                </div>
+                <h1 className="card__title">Interspace</h1>
+              </a>
+              <a href="/" className="card">
+                <div className="card__genre">Fantasy</div>
+                <div className="card__details">
+                  <div className="card__duration">
+                    <img src={duration} alt="duration" />
+                    <span>1hr 24mins</span>
+                  </div>
+                  <div className="card__views">
+                    <img src={views} alt="views" />
+                    <span>21.3K views</span>
+                  </div>
+                </div>
+                <h1 className="card__title">Interspace</h1>
+              </a>
+              <a href="/" className="card">
+                <div className="card__genre">Fantasy</div>
+                <div className="card__details">
+                  <div className="card__duration">
+                    <img src={duration} alt="duration" />
+                    <span>1hr 24mins</span>
+                  </div>
+                  <div className="card__views">
+                    <img src={views} alt="views" />
+                    <span>21.3K views</span>
+                  </div>
+                </div>
+                <h1 className="card__title">Interspace</h1>
+              </a>
+              <a href="/" className="card">
+                <div className="card__genre">Fantasy</div>
+                <div className="card__details">
+                  <div className="card__duration">
+                    <img src={duration} alt="duration" />
+                    <span>1hr 24mins</span>
+                  </div>
+                  <div className="card__views">
+                    <img src={views} alt="views" />
+                    <span>21.3K views</span>
+                  </div>
+                </div>
+                <h1 className="card__title">Interspace</h1>
+              </a>
+              <a href="/" className="card">
+                <div className="card__genre">Fantasy</div>
+                <div className="card__details">
+                  <div className="card__duration">
+                    <img src={duration} alt="duration" />
+                    <span>1hr 24mins</span>
+                  </div>
+                  <div className="card__views">
+                    <img src={views} alt="views" />
+                    <span>21.3K views</span>
+                  </div>
+                </div>
+                <h1 className="card__title">Interspace</h1>
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="footer">
+        <div className="container">
+          <a href="/" className="footer__logo">
+            <img src={logo} alt="logo" />
+            <span>MovieHub</span>
+          </a>
+          <span>Enjoy</span>
+        </div>
+      </footer>
     </>
   );
 };
