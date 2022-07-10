8cab422a4b8482b84b813150c170a247b4800c7e
Yyyurii <yurii.kryvko@gmail.com>
2022-07-10 12:34:41 +0300

    Created handler for burger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,30 @@
 import "./navbar.scss";
 
+import { useState } from "react";
+
 const Navbar = () => {
+  const [isShowBurger, setIsShowBurger] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsShowBurger((isShowBurger) => !isShowBurger);
+  };
+
   return (
     <>
-      <div className="burger">
+      <div
+        className={isShowBurger ? "burger active" : "burger"}
+        onClick={handleClick}
+      >
         <span></span>
         <span></span>
         <span></span>
       </div>
 
       <nav className="menu">
-        <ul className="menu__list">
+        <ul
+          className={isShowBurger ? "menu__list burger-active" : "menu__list"}
+        >
           <li className="menu__item">
             <a href="/" className="menu__link">
               Movies
