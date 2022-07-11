b2d8a2d5981881d96e7b89c97a27d7a6ad5ad683
Yyyurii <yurii.kryvko@gmail.com>
2022-07-11 15:51:03 +0300

    Created animation for progressbar on hover

--- src/components/PreviewsPlayBtn/PreviewsPlayBtn.jsx
+++ src/components/PreviewsPlayBtn/PreviewsPlayBtn.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import {
   CircularProgressbarWithChildren,
   buildStyles,
@@ -10,11 +12,22 @@ import "./previewsPlayBtn.scss";
 import triangle from "../../assets/images/icon/triangle.svg";
 
 const PreviewsPlayBtn = () => {
+  const [isHover, setIsHover] = useState(false);
+  const [progressbarWidth, setProgressbarWidth] = useState(0);
+
+  useEffect(() => {
+    isHover ? setProgressbarWidth(100) : setProgressbarWidth(0);
+  }, [isHover]);
+
   return (
-    <div className="play-btn">
+    <div
+      className="play-btn"
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
       <div style={{ fontSize: 12, marginTop: -5, width: 144 }}>
         <CircularProgressbarWithChildren
-          value={75}
+          value={progressbarWidth}
           styles={buildStyles({
             pathColor: `rgba(254, 213, 48, 1)`,
           })}
