1a8c786a36bdb0801474eb8733edcfef335800f5
Yyyurii <yurii.kryvko@gmail.com>
2022-08-26 21:24:16 +0300

    Changed render Link in TopicSectionHeader

--- src/components/TopicSectionHeader/TopicSectionHeader.jsx
+++ src/components/TopicSectionHeader/TopicSectionHeader.jsx
@@ -1,19 +1,24 @@
 import "./topicSectionHeader.scss";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useContext } from "react";
 import AppContext from "../../context";
 
-const TopicSectionHeader = ({title, isMovie}) => {
+const TopicSectionHeader = ({ title, isMovie }) => {
   const { onButtonClick } = useContext(AppContext);
-  const path = isMovie ? '/MovieHub/movies' : '/MovieHub/tv-shows';
 
   return (
     <div className="topic-section__header">
-      <div className="topic-section__title">
-        {title}
-      </div>
-      <Link to={path} onClick={onButtonClick}>View More</Link>
+      <div className="topic-section__title">{title}</div>
+      {isMovie ? (
+        <Link to="/MovieHub/movies" onClick={onButtonClick}>
+          View More
+        </Link>
+      ) : (
+        <Link to="/MovieHub/tv-shows" onClick={onButtonClick}>
+          View More
+        </Link>
+      )}
     </div>
   );
 };
